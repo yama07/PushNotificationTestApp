80b1a7926c89f08c30008d601d19e70d1184cc7d
yamamoto.nao-07 <yamamoto.nao-07@fujitsu.com>
2020-03-26 13:39:57 +0900

    Initial Commit

---
+++ PushNotificationTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PushNotificationTestApp
+//
+//  Created by Yamamoto Naoki on 2020/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
